bfc19bba9e66b840ef5c302b00d42bf2b5cfd6be
rafaelzelone <82285284+rafaelzelone@users.noreply.github.com>
2026-01-19 14:10:06 -0300

    new item

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+app.options("*", cors());
+
 
 app.use(express.json());
 app.use(routes);
